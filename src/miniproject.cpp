c8cf77a06dd00f006c54f5e72a21ac383cda0419
Elias Ben Slama <ebensl20@student.aau.dk>
2021-01-06 11:54:20 +0100

    Update

--- src/miniproject.cpp
+++ src/miniproject.cpp
@@ -91,10 +91,7 @@ int main(int argc, char** argv){ //
 /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
+   
    * You must call one of the versions of ros::init() before using any other
    * part of the ROS system.
    */
@@ -107,15 +104,10 @@ int main(int argc, char** argv){ //
 
   /**
    * The subscribe() call is how you tell ROS that you want to receive messages
-   * on a given topic.  This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing.
-   * and the first paramter is what topic you're subscribing to
+   * on a given topic. 
    *
    * The second parameter to the subscribe() function is the size of the message
-   * queue.  If messages are arriving faster than they are being processed, this
-   * is the number of messages that will be buffered up before beginning to throw
-   * away the oldest ones.
+   * queue.
    */
    ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
 
